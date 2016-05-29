b69469824ab93f6519537190824025ccd4832dba
Quentin LITZLER <qlitzler@gmail.com>
2016-05-29 18:43:46 +0200

    backup

--- app/src/main/java/qlitzler/com/opengl/GLRendererMain.java
+++ app/src/main/java/qlitzler/com/opengl/GLRendererMain.java
@@ -12,17 +12,22 @@ import javax.microedition.khronos.opengles.GL10;
  */
 public class GLRendererMain implements GLSurfaceView.Renderer {
 
-	private Square square;
+	private Square square1, square2;
 
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		GLES30.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-		square = new Square();
+		square2 = new Square(Square.COORDS2);
+		square1 = new Square(Square.COORDS1);
 	}
 
 	public void onDrawFrame(GL10 unused) {
 		GLES30.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-		square.draw();
+		float[] black = {0.0f, 0.0f, 0.0f, 0.0f};
+		float[] grey = {0.5f, 0.5f, 0.5f, 0.5f};
+
+		square1.draw(black);
+		square2.draw(grey);
 	}
 
 	public void onSurfaceChanged(GL10 unused, int width, int height) {
--- app/src/main/java/qlitzler/com/opengl/Square.java
+++ app/src/main/java/qlitzler/com/opengl/Square.java
@@ -10,36 +10,63 @@ import java.nio.ShortBuffer;
  */
 public class Square extends GLObject {
 
-	private FloatBuffer vbo;
-	private ShortBuffer ebo;
+	private int vao;
+	private int vbo;
+	private int ebo;
+	private int cbo;
+
+	private final int position;
+	private final int color;
+
+	private FloatBuffer vertices;
+	private ShortBuffer elements;
 
 	private static final int DIMENSIONS = 3;
 	private static final int STRIDE = DIMENSIONS * 4;
 
-	private static final float COORDS[] = {
+	public static final float COORDS1[] = {
+		-0.2f, 0.2f, 0.0f,
+		-0.2f, -0.2f, 0.0f,
+		0.2f, -0.2f, 0.0f,
+		0.2f, 0.2f, 0.0f
+	};
+
+	public static final float COORDS2[] = {
 		-0.5f, 0.5f, 0.0f,
 		-0.5f, -0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
 		0.5f, 0.5f, 0.0f
 	};
+
 	private static final short ELEMENTS[] = {0, 1, 2, 0, 2, 3};
 
-	public Square() {
+	public Square(float[] coords) {
 		super();
-		vbo = generateFloatBuffer(COORDS, COORDS.length * 4);
-		ebo = generateShortBuffer(ELEMENTS, ELEMENTS.length * 2);
+		position = GLES30.glGetAttribLocation(shader, "vPosition");
+		color = GLES30.glGetUniformLocation(shader, "vColor");
+
+		vertices = generateFloatBuffer(coords, coords.length * 4);
+		elements = generateShortBuffer(ELEMENTS, ELEMENTS.length * 2);
+
+//		IntBuffer intBuffer = IntBuffer.allocate();
+
+//		GLES30.glGenVertexArrays(1, vao, 0);
+		GLES30.glBindVertexArray(vao);
+		GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, vbo);
+		GLES30.glBufferData(GLES30.GL_ARRAY_BUFFER, coords.length, vertices, GLES30.GL_STATIC_DRAW);
+		GLES30.glVertexAttribPointer(position, DIMENSIONS, GLES30.GL_FLOAT, false, STRIDE, vertices);
+
+		GLES30.glBindVertexArray(0);
+		GLES30.glBindBuffer(GLES30.GL_ARRAY_BUFFER, 0);
 	}
 
-	public void draw() {
-		final float[] black = {0.0f, 0.0f, 0.0f, 0.0f};
-		final int position = GLES30.glGetAttribLocation(shader, "vPosition");
-		final int color = GLES30.glGetUniformLocation(shader, "vColor");
+	public void draw(float[] floats) {
 
 		GLES30.glUseProgram(shader);
 		GLES30.glEnableVertexAttribArray(position);
-		GLES30.glVertexAttribPointer(position, DIMENSIONS, GLES30.GL_FLOAT, false, STRIDE, vbo);
-		GLES30.glUniform4fv(color, 1, black, 0);
-		GLES30.glDrawElements(GLES30.GL_TRIANGLE_STRIP, ELEMENTS.length, GLES30.GL_UNSIGNED_SHORT, ebo);
+		GLES30.glUniform4fv(color, 1, floats, 0);
+		GLES30.glBindVertexArray(vao);
+		GLES30.glDrawElements(GLES30.GL_TRIANGLE_STRIP, ELEMENTS.length, GLES30.GL_UNSIGNED_SHORT, elements);
 		GLES30.glDisableVertexAttribArray(position);
 	}
 
